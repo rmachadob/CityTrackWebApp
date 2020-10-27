0d049c92688507333ad58894b54dc16487220a6d
Ricardo barbosa <ricardomachado2112@gmail.comgit config --global user.name rmachadobgit config --global user.email se>
2020-10-27 18:55:45 -0300

    montando a tela

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector:'app-post-list',
-  templateUrl: './post-list.component.html'
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent{}
